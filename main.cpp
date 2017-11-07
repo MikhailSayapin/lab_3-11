990652609761505f21cc61643cc0d27d31ff2c7f
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2017-11-07 22:11:24 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include "header1.h"
+#include "header2.h"
+
+using namespace std;
+using namespace ns1;
+using namespace ns2;
+
+int main ()
+{ 
+  cout << "n:" << n << endl;
+  cout << "m:" << m;
+}
